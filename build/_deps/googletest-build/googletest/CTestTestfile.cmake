419c4437d1b8e40033f19c6062256e1a852253a6
Sam Brockie <sgb39@cam.ac.uk>
2022-01-22 11:07:05 +0000

    Remove build directory

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sambrockie/Documents/Code/Repositories/mlfs/build/_deps/googletest-src/googletest
-# Build directory: /Users/sambrockie/Documents/Code/Repositories/mlfs/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/tests[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( LayerTest.BasicAssertions /Users/sambrockie/Documents/Code/Repositories/mlfs/build/tests/tests [==[--gtest_filter=LayerTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties( LayerTest.BasicAssertions PROPERTIES WORKING_DIRECTORY /Users/sambrockie/Documents/Code/Repositories/mlfs/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( tests_TESTS LayerTest.BasicAssertions)
